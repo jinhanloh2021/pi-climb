1cdd67e6246f13eacb6139c5369b2a78e257d4e9
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2025-06-09 15:41:32 +0800

    feat: book-anything init

---
+++ initialisers/connectToDb.go
@@ -0,0 +1,23 @@
+package initialisers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDb() {
+	var err error
+
+	dsn := os.Getenv("DATABASE_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	fmt.Println("Successfully connected to the database!")
+}
