b2c433d7945c97ae15fcfeb2a0aa9f43203b6999
jinhan.loh.2021 <jinhan.loh.2021@scis.smu.edu.sg>
2025-06-26 16:29:41 +0800

    chore: Rename migrations, organise, fix multiple permissive policies warning

--- server/migrations/20250625080435.sql
+++ server/migrations/20250625080435.sql
@@ -1,11 +1,5 @@
 ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
 
--- Policy for SELECT: A user can SELECT their OWN profile.
-CREATE POLICY "Users can select their own profile"
-ON public.users
-FOR SELECT
-USING ((select auth.uid()) = supabase_id);
-
 -- Policy for UPDATE: A user can UPDATE their OWN profile.
 CREATE POLICY "Users can update their own profile"
 ON public.users
@@ -18,3 +12,11 @@ CREATE POLICY "Users can delete their own profile"
 ON public.users
 FOR DELETE
 USING ((select auth.uid()) = supabase_id);
+
+-- Policy for SELECT: A user can SELECT all other profiles.
+CREATE POLICY "Users can read all users"
+ON public.users
+AS PERMISSIVE
+FOR SELECT
+TO authenticated
+USING (true);
--- server/migrations/20250625093810.sql
+++
@@ -1,6 +0,0 @@
-CREATE POLICY "Enable read access for all users"
-ON public.users
-AS PERMISSIVE
-FOR SELECT
-TO authenticated
-USING (true);
