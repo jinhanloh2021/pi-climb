00d75baca2538acd226793ea1d468b814411ab06
jinhanloh2021 <jinhan.loh.2021@scis.smu.edu.sg>
2025-07-12 10:06:06 +0800

    feat: Make auth redirections back to root route, conditional rendering on root route for logged-in vs non-logged-in user

--- web/app/protected/layout.tsx
+++ web/app/protected/layout.tsx
@@ -1,9 +1,7 @@
-import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
 
 export default function ProtectedLayout({
   children,
@@ -15,12 +13,7 @@ export default function ProtectedLayout({
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
-            </div>
+            <div className="flex gap-5 items-center font-semibold"></div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
         </nav>
@@ -29,17 +22,6 @@ export default function ProtectedLayout({
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
           <ThemeSwitcher />
         </footer>
       </div>
