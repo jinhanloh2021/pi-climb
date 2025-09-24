d950c2ac46fe1d6af8c3951df2523c5a1bb51ea9
jinhanloh2021 <jinhan.loh.2021@scis.smu.edu.sg>
2025-09-24 14:11:24 +0800

    bug: fix null error on feed page

--- web/app/(protected)/feed/page.tsx
+++ web/app/(protected)/feed/page.tsx
@@ -8,10 +8,11 @@ export default async function FeedPage() {
   return (
     <div className="flex flex-col gap-12">
       <h2 className="font-bold text-2xl mb-4">Feed</h2>
-      {posts.posts.map(async (p) => {
+      {posts.posts?.map(async (p) => {
         const liked = await PostService.myLikedPost(p.id.toString());
         return <FeedPost key={p.id} post={p} liked={liked.liked} />;
       })}
+      {(!posts.posts || posts.posts.length == 0) && <p>No posts</p>}
     </div>
   );
 }
