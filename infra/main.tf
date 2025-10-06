c54f54b1b6eec02dd376f7732882b83882d45b82
jinhan.loh.2021 <jinhan.loh.2021@scis.smu.edu.sg>
2025-10-07 03:27:40 +0800

    bug: Fix default security group issue, use variables for SG in vpc_endpoint. Fix nat gateway static eni bug

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,3 @@
-// todo: Module orchestration with terraform
 // todo: Terraform import to generate state files. S3 storage
 // Use SSM to get image tag of latest image for terraform
 // Inline task-definition.json. Find online example on github, copy
