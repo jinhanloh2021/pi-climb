b04bb66f68a5aa194323c10b20ba65380a8f437f
jinhan.loh.2021 <jinhan.loh.2021@scis.smu.edu.sg>
2025-10-08 20:33:51 +0800

    feat: Add route53 state management, fix issue with dynamic alb dns. Add s3 storage for tf state locking. Use dynamic image tag for ecs task definition

--- infra/generated/aws/ecs/ecs_task_definition.tf
+++ infra/generated/aws/ecs/ecs_task_definition.tf
@@ -1,18 +1,81 @@
+data "aws_ssm_parameter" "server_image_uri" {
+  name = "/pi-climb/dev/server-image-uri"
+}
+
+data "aws_ssm_parameter" "web_image_uri" {
+  name = "/pi-climb/dev/web-image-uri"
+}
+
 resource "aws_ecs_task_definition" "tfer--task-definition-002F-pi-climb_task" {
-  container_definitions    = "[{\"environment\":[],\"essential\":true,\"image\":\"842832773369.dkr.ecr.ap-southeast-1.amazonaws.com/pi-climb-dev-server:d3584cba4f52d6da9f453109bdd05bc4e5f3429d\",\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-create-group\":\"true\",\"awslogs-region\":\"ap-southeast-1\",\"awslogs-stream-prefix\":\"ecs\",\"awslogs-group\":\"/ecs/pi-climb_task\"}},\"mountPoints\":[],\"name\":\"go_server\",\"portMappings\":[{\"appProtocol\":\"http\",\"containerPort\":8080,\"hostPort\":8080,\"name\":\"go_server-8080-tcp\",\"protocol\":\"tcp\"}],\"secrets\":[{\"name\":\"DATABASE_URL\",\"valueFrom\":\"arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:DATABASE_URL::\"},{\"name\":\"JWT_SECRET\",\"valueFrom\":\"arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:JWT_SECRET::\"},{\"name\":\"SUPABASE_AUTH_EXTERNAL_GOOGLE_CLIENT_ID\",\"valueFrom\":\"arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:SUPABASE_AUTH_EXTERNAL_GOOGLE_CLIENT_ID::\"},{\"name\":\"SUPABASE_AUTH_EXTERNAL_GOOGLE_SECRET\",\"valueFrom\":\"arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:SUPABASE_AUTH_EXTERNAL_GOOGLE_SECRET::\"}],\"systemControls\":[],\"volumesFrom\":[]},{\"environment\":[],\"essential\":true,\"image\":\"842832773369.dkr.ecr.ap-southeast-1.amazonaws.com/pi-climb-dev-web:d3584cba4f52d6da9f453109bdd05bc4e5f3429d\",\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-stream-prefix\":\"ecs\",\"awslogs-group\":\"/ecs/pi-climb_task\",\"awslogs-create-group\":\"true\",\"awslogs-region\":\"ap-southeast-1\"}},\"mountPoints\":[],\"name\":\"nextjs_server\",\"portMappings\":[{\"appProtocol\":\"http\",\"containerPort\":3000,\"hostPort\":3000,\"name\":\"nextjs_server-3000-tcp\",\"protocol\":\"tcp\"}],\"secrets\":[{\"name\":\"NEXT_PUBLIC_API_URL\",\"valueFrom\":\"arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:NEXT_PUBLIC_API_URL::\"},{\"name\":\"NEXT_PUBLIC_SUPABASE_ANON_KEY\",\"valueFrom\":\"arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:NEXT_PUBLIC_SUPABASE_ANON_KEY::\"},{\"name\":\"NEXT_PUBLIC_SUPABASE_URL\",\"valueFrom\":\"arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:NEXT_PUBLIC_SUPABASE_URL::\"}],\"systemControls\":[],\"volumesFrom\":[]}]"
-  cpu                      = "256"
-  enable_fault_injection   = "false"
-  execution_role_arn       = "arn:aws:iam::842832773369:role/ecsTaskExecutionRole"
   family                   = "pi-climb_task"
-  memory                   = "1024"
   network_mode             = "awsvpc"
-  region                   = "ap-southeast-1"
   requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "1024"
+  execution_role_arn       = "arn:aws:iam::842832773369:role/ecsTaskExecutionRole"
+
+  container_definitions = jsonencode([
+    {
+      name      = "go_server"
+      essential = true
+      image     = data.aws_ssm_parameter.server_image_uri.value
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+          appProtocol   = "http"
+          name          = "go_server-8080-tcp"
+        }
+      ]
+      secrets = [
+        { name = "DATABASE_URL", valueFrom = "arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:DATABASE_URL::" },
+        { name = "JWT_SECRET", valueFrom = "arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:JWT_SECRET::" },
+        { name = "SUPABASE_AUTH_EXTERNAL_GOOGLE_CLIENT_ID", valueFrom = "arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:SUPABASE_AUTH_EXTERNAL_GOOGLE_CLIENT_ID::" },
+        { name = "SUPABASE_AUTH_EXTERNAL_GOOGLE_SECRET", valueFrom = "arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:SUPABASE_AUTH_EXTERNAL_GOOGLE_SECRET::" }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/pi-climb_task"
+          "awslogs-region"        = "ap-southeast-1"
+          "awslogs-stream-prefix" = "ecs"
+          "awslogs-create-group"  = "true"
+        }
+      }
+    },
+    {
+      name      = "nextjs_server"
+      essential = true
+      image     = data.aws_ssm_parameter.web_image_uri.value
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+          protocol      = "tcp"
+          appProtocol   = "http"
+          name          = "nextjs_server-3000-tcp"
+        }
+      ]
+      secrets = [
+        { name = "NEXT_PUBLIC_API_URL", valueFrom = "arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:NEXT_PUBLIC_API_URL::" },
+        { name = "NEXT_PUBLIC_SUPABASE_ANON_KEY", valueFrom = "arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:NEXT_PUBLIC_SUPABASE_ANON_KEY::" },
+        { name = "NEXT_PUBLIC_SUPABASE_URL", valueFrom = "arn:aws:secretsmanager:ap-southeast-1:842832773369:secret:pi-climb/dev-wXQQeQ:NEXT_PUBLIC_SUPABASE_URL::" }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/pi-climb_task"
+          "awslogs-region"        = "ap-southeast-1"
+          "awslogs-stream-prefix" = "ecs"
+          "awslogs-create-group"  = "true"
+        }
+      }
+    }
+  ])
 
   runtime_platform {
     cpu_architecture        = "X86_64"
     operating_system_family = "LINUX"
   }
-
-  track_latest = "false"
 }
